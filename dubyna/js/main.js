00b5896a68041121ede90976dcab2edd74539601
freecree <bodya20126@gmail.com>
2021-02-14 16:20:13 +0200

    update
    update

--- dubyna/js/main.js
+++ dubyna/js/main.js
@@ -1,32 +1,27 @@
 (function($) {
-    $(function() {
+    $('.navbar-button').on('click', e => {
+        //console.log("Logo");
+        // $('.navbar').css('display', 'block');
+        $('.navbar').toggleClass('navbar-mobile');
+        console.log("Logo");
+    });
+    $(document).click( function(e){
+        // if(window.screen.availWidth < 992 + 'px') {
+            if ($(e.target).closest('.navbar-button').length) {
+                // клик внутри элемента
+                return;
+            }
+            // клик снаружи элемента
+            $('.navbar').removeClass('navbar-mobile');
+        // }
+    });
 
+    $(function() {
         $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-            console.log("click: " + $(this).className);
+            console.log("click");
             $(this)
                 .addClass('active').siblings().removeClass('active')
                 .closest('div.products__tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
         });
-        $('.navbar-button').on('click', e => {
-            console.log("Logo");
-            $('#navbar').css('display', 'block');
-            console.log("Logo");
-        });
-
-        $('.gamburger_wrapper').click(function(event) {
-            $(this).parent('.navbar').toggleClass('navbar_active');
-        });
-
-        $(document).click( function(e){
-            if(window.screen.availWidth < 992 + 'px') {
-                if ($(e.target).closest('.navbar-button').length) {
-                    // клик внутри элемента
-                    return;
-                }
-                // клик снаружи элемента
-                $('#navbar').fadeOut();
-            }
-        });
-
     });
 })(jQuery);
