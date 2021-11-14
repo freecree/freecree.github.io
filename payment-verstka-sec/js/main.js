e885f812a88606bcb68c69ec05d53bb03ee74cda
freecree <bodya20126@gmail.com>
2021-11-14 15:46:38 +0200

    changed

--- payment-verstka-sec/js/main.js
+++ payment-verstka-sec/js/main.js
@@ -1,108 +1,20 @@
-let rangers = document.getElementsByClassName("payment-period__range-inp");
-let periods = document.getElementsByClassName("payment-period__month");
-let paymentWays = document.getElementsByClassName("payment__stage-block");
-let paymentWaysContents = document.getElementsByClassName("payment__content-block");
-let accordionHeaders = document.getElementsByClassName("payment-way-accordion__header");
-
-let rangerVal = 0;
-
-/* Аккордион на мобилке */
-for (let i = 0; i < accordionHeaders.length; i++) {
-    accordionHeaders[i].onclick = function () {
-        if (accordionHeaders[i].closest(".payment-way-accordion").classList.contains("opened")) {
-            accordionHeaders[i].closest(".payment-way-accordion").classList.remove("opened");
-            console.log("if1");
-        } else {
-            for (let i = 0; i < accordionHeaders.length; i++) {
-                accordionHeaders[i].closest(".payment-way-accordion").classList.remove("opened");
-            }
-            accordionHeaders[i].closest(".payment-way-accordion").classList.add("opened");
-            console.log("else");
-        }
-
+// eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('4 u=h.c("6-W-U");4 8=h.c("6-q");4 l=h.c("14-J");4 H=h.c("B-J");4 f=h.c("6-m-10");4 5=0;9(4 i=0;i<f.e;i++){f[i].y=g(){A(f[i].k(".6-m-v").7.11("w")){f[i].k(".6-m-v").7.o("w");N.M("12")}z{9(4 i=0;i<f.e;i++){f[i].k(".6-m-v").7.o("w")}f[i].k(".6-m-v").7.r("w");N.M("z")}}}9(4 i=0;i<l.e;i++){l[i].y=g(){9(4 j=0;j<l.e;j++){H[j].7.o(\'n\');l[j].7.o(\'n\')}H[i].7.r(\'n\');l[i].7.r(\'n\')}}4 F=h.c("G-L-Z");9(4 i=0;i<F.e;i++){F[i].y=g(){4 D=h.c("G-m");t.k(\'.G-L-r\').O.P=\'15\';9(4 i=0;i<D.e;i++){D[i].O.P=\'J\'}}}g Y(a,b){4 8=a.c("6-q");4 T=8[b].p(\'.6-q-K\').C;4 R=8[b].p(\'.6-q-K\').19.Q;a.p(".1a-Q").C=R;a.p(".18-17").C=T}g s(a,b){4 8=a.c("6-q");9(4 i=0;i<8.e;i++){8[i].7.o(\'n\')}8[b].7.r(\'n\');Y(a,b)}9(4 i=0;i<8.e;i++){8[i].y=g(){4 I=i%3;5=I*x;4 d=t.k(".B-d");d.p(".6-W-U").E=5;s(d,I)}}9(4 i=0;i<u.e;i++){u[i].16=g(){4 d=t.k(".B-d");5=t.E;A(5>=0&&5<S){5=0;s(d,0)}z A((5>=S&&5<x)||(5>=x&&5<X)){5=x;s(d,1)}z A(5>=X&&5<=V){5=V;s(d,2)}};u[i].13=g(){t.E=5}}',62,73,'||||let|rangerVal|payment|classList|periods|for|||getElementsByClassName|wrapper|length|accordionHeaders|function|document|||closest|paymentWays|way|active|remove|querySelector|period__month|add|makeActivePeriod|this|rangers|accordion|opened|50|onclick|else|if|payment__content|innerHTML|addedWays|value|addedWaysLinks|payment__another|paymentWaysContents|index|block|txt|ways|log|console|style|display|price|priceVal|25|monthVal|inp|100|period__range|75|setActivePeriodData|link|accordion__header|contains|if1|onchange|payment__stage|none|oninput|amount|payment__month|dataset|payment__period'.split('|'),0,{}))
+var _0xa384=["\x70\x61\x79\x6D\x65\x6E\x74\x2D\x70\x65\x72\x69\x6F\x64\x5F\x5F\x72\x61\x6E\x67\x65\x2D\x69\x6E\x70","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x43\x6C\x61\x73\x73\x4E\x61\x6D\x65","\x70\x61\x79\x6D\x65\x6E\x74\x2D\x70\x65\x72\x69\x6F\x64\x5F\x5F\x6D\x6F\x6E\x74\x68","\x70\x61\x79\x6D\x65\x6E\x74\x5F\x5F\x73\x74\x61\x67\x65\x2D\x62\x6C\x6F\x63\x6B","\x70\x61\x79\x6D\x65\x6E\x74\x5F\x5F\x63\x6F\x6E\x74\x65\x6E\x74\x2D\x62\x6C\x6F\x63\x6B","\x70\x61\x79\x6D\x65\x6E\x74\x2D\x77\x61\x79\x2D\x61\x63\x63\x6F\x72\x64\x69\x6F\x6E\x5F\x5F\x68\x65\x61\x64\x65\x72","\x6C\x65\x6E\x67\x74\x68","\x6F\x6E\x63\x6C\x69\x63\x6B","\x6F\x70\x65\x6E\x65\x64","\x63\x6F\x6E\x74\x61\x69\x6E\x73","\x63\x6C\x61\x73\x73\x4C\x69\x73\x74","\x2E\x70\x61\x79\x6D\x65\x6E\x74\x2D\x77\x61\x79\x2D\x61\x63\x63\x6F\x72\x64\x69\x6F\x6E","\x63\x6C\x6F\x73\x65\x73\x74","\x72\x65\x6D\x6F\x76\x65","\x69\x66\x31","\x6C\x6F\x67","\x61\x64\x64","\x65\x6C\x73\x65","\x61\x63\x74\x69\x76\x65","\x70\x61\x79\x6D\x65\x6E\x74\x5F\x5F\x61\x6E\x6F\x74\x68\x65\x72\x2D\x77\x61\x79\x73\x2D\x6C\x69\x6E\x6B","\x70\x61\x79\x6D\x65\x6E\x74\x5F\x5F\x61\x6E\x6F\x74\x68\x65\x72\x2D\x77\x61\x79","\x64\x69\x73\x70\x6C\x61\x79","\x73\x74\x79\x6C\x65","\x2E\x70\x61\x79\x6D\x65\x6E\x74\x5F\x5F\x61\x6E\x6F\x74\x68\x65\x72\x2D\x77\x61\x79\x73\x2D\x61\x64\x64","\x6E\x6F\x6E\x65","\x62\x6C\x6F\x63\x6B","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x2E\x70\x61\x79\x6D\x65\x6E\x74\x2D\x70\x65\x72\x69\x6F\x64\x5F\x5F\x6D\x6F\x6E\x74\x68\x2D\x74\x78\x74","\x71\x75\x65\x72\x79\x53\x65\x6C\x65\x63\x74\x6F\x72","\x70\x72\x69\x63\x65","\x64\x61\x74\x61\x73\x65\x74","\x2E\x70\x61\x79\x6D\x65\x6E\x74\x5F\x5F\x70\x65\x72\x69\x6F\x64\x2D\x70\x72\x69\x63\x65","\x2E\x70\x61\x79\x6D\x65\x6E\x74\x5F\x5F\x6D\x6F\x6E\x74\x68\x2D\x61\x6D\x6F\x75\x6E\x74","\x2E\x70\x61\x79\x6D\x65\x6E\x74\x5F\x5F\x63\x6F\x6E\x74\x65\x6E\x74\x2D\x77\x72\x61\x70\x70\x65\x72","\x76\x61\x6C\x75\x65","\x2E\x70\x61\x79\x6D\x65\x6E\x74\x2D\x70\x65\x72\x69\x6F\x64\x5F\x5F\x72\x61\x6E\x67\x65\x2D\x69\x6E\x70","\x6F\x6E\x69\x6E\x70\x75\x74","\x6F\x6E\x63\x68\x61\x6E\x67\x65"];let rangers=document[_0xa384[1]](_0xa384[0]);let periods=document[_0xa384[1]](_0xa384[2]);let paymentWays=document[_0xa384[1]](_0xa384[3]);let paymentWaysContents=document[_0xa384[1]](_0xa384[4]);let accordionHeaders=document[_0xa384[1]](_0xa384[5]);let rangerVal=0;for(let i=0;i< accordionHeaders[_0xa384[6]];i++){accordionHeaders[i][_0xa384[7]]= function(){if(accordionHeaders[i][_0xa384[12]](_0xa384[11])[_0xa384[10]][_0xa384[9]](_0xa384[8])){accordionHeaders[i][_0xa384[12]](_0xa384[11])[_0xa384[10]][_0xa384[13]](_0xa384[8]);console[_0xa384[15]](_0xa384[14])}else {for(let i=0;i< accordionHeaders[_0xa384[6]];i++){accordionHeaders[i][_0xa384[12]](_0xa384[11])[_0xa384[10]][_0xa384[13]](_0xa384[8])};accordionHeaders[i][_0xa384[12]](_0xa384[11])[_0xa384[10]][_0xa384[16]](_0xa384[8]);console[_0xa384[15]](_0xa384[17])}}};for(let i=0;i< paymentWays[_0xa384[6]];i++){paymentWays[i][_0xa384[7]]= function(){for(let _0x6e51x8=0;_0x6e51x8< paymentWays[_0xa384[6]];_0x6e51x8++){paymentWaysContents[_0x6e51x8][_0xa384[10]][_0xa384[13]](_0xa384[18]);paymentWays[_0x6e51x8][_0xa384[10]][_0xa384[13]](_0xa384[18])};paymentWaysContents[i][_0xa384[10]][_0xa384[16]](_0xa384[18]);paymentWays[i][_0xa384[10]][_0xa384[16]](_0xa384[18])}};let addedWaysLinks=document[_0xa384[1]](_0xa384[19]);for(let i=0;i< addedWaysLinks[_0xa384[6]];i++){addedWaysLinks[i][_0xa384[7]]= function(){let _0x6e51xa=document[_0xa384[1]](_0xa384[20]);this[_0xa384[12]](_0xa384[23])[_0xa384[22]][_0xa384[21]]= _0xa384[24];for(let i=0;i< _0x6e51xa[_0xa384[6]];i++){_0x6e51xa[i][_0xa384[22]][_0xa384[21]]= _0xa384[25]}}};function setActivePeriodData(_0x6e51xc,_0x6e51xd){let periods=_0x6e51xc[_0xa384[1]](_0xa384[2]);let _0x6e51xe=periods[_0x6e51xd][_0xa384[28]](_0xa384[27])[_0xa384[26]];let _0x6e51xf=periods[_0x6e51xd][_0xa384[28]](_0xa384[27])[_0xa384[30]][_0xa384[29]];_0x6e51xc[_0xa384[28]](_0xa384[31])[_0xa384[26]]= _0x6e51xf;_0x6e51xc[_0xa384[28]](_0xa384[32])[_0xa384[26]]= _0x6e51xe}function makeActivePeriod(_0x6e51xc,_0x6e51xd){let periods=_0x6e51xc[_0xa384[1]](_0xa384[2]);for(let i=0;i< periods[_0xa384[6]];i++){periods[i][_0xa384[10]][_0xa384[13]](_0xa384[18])};periods[_0x6e51xd][_0xa384[10]][_0xa384[16]](_0xa384[18]);setActivePeriodData(_0x6e51xc,_0x6e51xd)}for(let i=0;i< periods[_0xa384[6]];i++){periods[i][_0xa384[7]]= function(){let _0x6e51xd=i% 3;rangerVal= _0x6e51xd* 50;let _0x6e51xc=this[_0xa384[12]](_0xa384[33]);_0x6e51xc[_0xa384[28]](_0xa384[35])[_0xa384[34]]= rangerVal;makeActivePeriod(_0x6e51xc,_0x6e51xd)}};for(let i=0;i< rangers[_0xa384[6]];i++){rangers[i][_0xa384[36]]= function(){let _0x6e51xc=this[_0xa384[12]](_0xa384[33]);rangerVal= this[_0xa384[34]];if(rangerVal>= 0&& rangerVal< 25){rangerVal= 0;makeActivePeriod(_0x6e51xc,0)}else {if((rangerVal>= 25&& rangerVal< 50)|| (rangerVal>= 50&& rangerVal< 75)){rangerVal= 50;makeActivePeriod(_0x6e51xc,1)}else {if(rangerVal>= 75&& rangerVal<= 100){rangerVal= 100;makeActivePeriod(_0x6e51xc,2)}}}};rangers[i][_0xa384[37]]= function(){this[_0xa384[34]]= rangerVal}}
+//alert(navigator.userAgent);
+let androidBlocks = document.getElementsByClassName("for-adroid");
+let appleBlocks = document.getElementsByClassName("for-apple");
+let arr = [1, 2, 3]
+function showAndHide(toShow, toHide) {
+    for(let element of toShow) {
+        element.style.display = "block";
+    }
+    for(let element of toHide) {
+        element.style.display = "none";
     }
 }
-
-/*Выбор способа оплаты */
-for (let i = 0; i < paymentWays.length; i++) {
-    paymentWays[i].onclick = function () {
-        for (let j = 0; j < paymentWays.length; j++) {
-            paymentWaysContents[j].classList.remove('active');
-            paymentWays[j].classList.remove('active');
-        }
-        paymentWaysContents[i].classList.add('active');
-        paymentWays[i].classList.add('active');
-    }
+if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    showAndHide(appleBlocks, androidBlocks);    
+} else if (/Android|webOSBlackBerry/i.test(navigator.userAgent)) {
+    //showAndHide(androidBlocks, appleBlocks);
+    showAndHide(appleBlocks, androidBlocks);    
 }
-let addedWaysLinks = document.getElementsByClassName("payment__another-ways-link");
-for (let i = 0; i < addedWaysLinks.length; i++) {
-    addedWaysLinks[i].onclick = function () {
-        let addedWays = document.getElementsByClassName("payment__another-way");
-        this.closest('.payment__another-ways-add').style.display = 'none';
-        for (let i = 0; i < addedWays.length; i++) {
-            addedWays[i].style.display = 'block';
-        }
-    }
-
-}
-
-/* Ползунок с периодами оплаты НАЧАЛО */
-/* Работает только для 3-х периодов оплаты!!! */
-function setActivePeriodData(wrapper, index) {
-    let periods = wrapper.getElementsByClassName("payment-period__month");
-    let monthVal = periods[index].querySelector('.payment-period__month-txt').innerHTML
-    let priceVal = periods[index].querySelector('.payment-period__month-txt').dataset.price;
-   
-    
-    wrapper.querySelector(".payment__period-price").innerHTML = priceVal;
-    wrapper.querySelector(".payment__month-amount").innerHTML = monthVal;
-}
-
-function makeActivePeriod(wrapper, index) {
-    let periods = wrapper.getElementsByClassName("payment-period__month");
-    for (let i = 0; i < periods.length; i++) {
-        periods[i].classList.remove('active');
-
-    }
-    periods[index].classList.add('active');
-    setActivePeriodData(wrapper, index);
-
-}
-
-for (let i = 0; i < periods.length; i++) {
-
-    periods[i].onclick = function () {
-        let index = i%3; //3 потому что 3 периода
-        rangerVal = index * 50;
-        
-        let wrapper = this.closest(".payment__content-wrapper");
-        wrapper.querySelector(".payment-period__range-inp").value = rangerVal;
-        makeActivePeriod(wrapper, index);
-    }
-}
-
-for (let i = 0; i < rangers.length; i++) {
-    rangers[i].oninput = function () {
-        let wrapper = this.closest(".payment__content-wrapper");
-        rangerVal = this.value;
-        if (rangerVal >= 0 && rangerVal < 25) {
-            rangerVal = 0;
-            makeActivePeriod(wrapper, 0);
-        } else if ((rangerVal >= 25 && rangerVal < 50)
-            || (rangerVal >= 50 && rangerVal < 75)) {
-            rangerVal = 50;
-            makeActivePeriod(wrapper, 1);
-        } else if (rangerVal >= 75 && rangerVal <= 100) {
-            rangerVal = 100;
-            makeActivePeriod(wrapper, 2);
-        }
-
-    }
-    rangers[i].onchange = function () {
-        this.value = rangerVal;
-    }
-}
-
-
-
-/* Ползунок с периодами оплаты КОНЕЦ */
